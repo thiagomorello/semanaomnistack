31fd449585626fda86e72c18290634c64930fcb6
Thiago Roieski Maltezo <thiago@Mac-mini-de-Thiago.local>
2020-01-16 09:46:49 -0300

    Inicio da comunicação com a API *Importante trocar o ip no services/api.js

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
-
+import { MaterialIcons } from '@expo/vector-icons';
 function Main( { navigation }){
 
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -33,6 +33,7 @@ function Main( { navigation }){
      return null;
    }
    return (
+    <>
      <MapView initialRegion={currentRegion} style={styles.map}>
          <Marker coordinate={{ latitude:-25.5069972, longitude:-54.550524}} >
             <Image style={styles.avatar} source={{ uri:'http://placehold.it/400x400' }} />
@@ -45,8 +46,21 @@ function Main( { navigation }){
                   <Text style={styles.devTechs}>Techs</Text>
                 </View>
             </Callout>
+
          </Marker>
      </MapView>
+     <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs"
+          placeholderTextColor="#999"
+          autoCapitalize="true"
+          autoCorrect= {false} />
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+     </View>
+   </>
    );
 }
 const styles = StyleSheet.create({
@@ -78,6 +92,40 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop:5,
   },
+  searchForm:{
+    position:'absolute',
+    top:20,
+    left:20,
+    right:20,
+    zIndex: 5,
+    flexDirection:'row'
+  },
+  searchInput:{
+    flex:1,
+    height:50,
+    backgroundColor:'#fff',
+    color:'#333',
+    borderRadius:25,
+    paddingHorizontal:20,
+    fontSize:16,
+    shadowColor:'#000',
+    shadowOpacity:0.2,
+    shadowOffset:{
+      width:4,
+      height:4,
+    },
+    elevation:2,
+  },
+  loadButton:{
+    width:50,
+    height:50,
+    backgroundColor:'#e30134',
+    borderRadius:25,
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:15,
+  }
+
 
 });
 export default Main;
